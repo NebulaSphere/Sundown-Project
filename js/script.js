aeb8bb1615b1ccd71003500c2e2ceba6ef2c3a4b
NebulaSphere <xtylish52212@gmail.com>
2024-01-26 06:01:11 +0500

    add bottom heading and right image and paragraph

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // locomotive scrolling like smooth scrolling
 
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
+    el: document.querySelector('#main'),
     smooth: true
 });
 
